5fc3a6d8058fe6b1051348bc88e109e8ded111f4
shreyartha-bioinfo <shreyartha@gmail.com>
2026-07-09 21:37:48 -0400

    Update SHEET_API_URL with the deployed URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Paste the Google Apps Script Web App URL here after deploying (see README.md).
 // Example: "https://script.google.com/macros/s/AKfycb.../exec"
 const CONFIG = {
-  SHEET_API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+  SHEET_API_URL: "https://script.google.com/macros/s/AKfycbwKGXZN0Mav-b6tosJE84kzH03fheN2O-krGiWamy9kOGEzbpzM1OnjB2fvDWGtmj05/exec"
 };
